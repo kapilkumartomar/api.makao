55b420bb510a88bf64b803099452d6910ec3b22d
dev-reactjs <dev.reactjs@gmail.com>
2024-02-06 10:29:27 +0530

    review routes, model, controller added.

---
+++ src/v1/review/review.controller.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+
+import { wentWrong } from "@util/helper";
+import { findIsReviewGiven, findReview, postReview } from "./review.resources";
+
+export async function handleGetReview(req: Request, res: Response) {
+  try {
+    const reviews: any = await findReview(req.query);
+
+    return res.status(200).json({
+      message: 'Reviews fetched successfully',
+      data: reviews,
+    });
+  } catch (err: any) {
+    return res.status(500).json({
+      message: err?.message ?? wentWrong,
+    });
+  }
+}
+
+export async function handleIsReviewGiven(req: Request, res: Response) {
+  try {
+    const review: any = await findIsReviewGiven(req.params);
+    return res.status(200).json({
+      message: 'given Review fetched successfully',
+      data: review,
+    });
+  } catch (err: any) {
+    return res.status(500).json({
+      message: err?.message ?? wentWrong,
+    });
+  }
+}
+
+export async function handlePostReview(req: Request, res: Response) {
+  try {
+    const reviews: any = await postReview(req.body);
+
+    return res.status(200).json({
+      message: "Reviews Posted successfully",
+      data: reviews,
+    });
+  } catch (err: any) {
+    return res.status(500).json({
+      message: err?.message ?? wentWrong,
+    });
+  }
+}
