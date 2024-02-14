c1854bb69046dda7ce00d570f558ff0188ec847b
dev-reactjs <dev.reactjs@gmail.com>
2024-02-14 19:17:00 +0530

    user model & user-trust api made.

--- src/v1/review/review.controller.ts
+++ src/v1/review/review.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 
 import { wentWrong } from '@util/helper';
 import { findIsReviewGiven, findReview, postReview } from './review.resources';
+import { findOneAndUpdateUser } from '../user/user.resources';
 
 export async function handleGetReview(req: Request, res: Response) {
   try {
@@ -36,7 +37,34 @@ export async function handleIsReviewGiven(req: Request, res: Response) {
 
 export async function handlePostReview(req: Request, res: Response) {
   try {
+    const { _id: userId } = req.body.userInfo;
     const reviews: any = await postReview(req.body);
+    const givenReview = reviews[0].review;
+    // user-trust-score logic
+    //   if (givenReview === 0) {
+    //     user.userTrustNote = Math.max(minTrustNote, currentTrustNote - (currentTrustNote * baseChangePercentage));
+    // } else if (givenReview === 1) {
+    //     user.userTrustNote = Math.min(maxTrustNote, currentTrustNote + (currentTrustNote * baseChangePercentage));
+    // }
+      
+    const maxTrustNote = 5; // Maximum trust note
+    const minTrustNote = 0; // Minimum trust note
+    const baseChangePercentage = 0.05;
+    const updateUserTrustNote: any = await findOneAndUpdateUser(userId, [
+      {
+        $set:
+        {
+          userTrustNote:
+          {
+            $cond: {
+              if: { $eq: [givenReview, 0] },
+              then: { $max: [minTrustNote, { $subtract: ['$userTrustNote', { $multiply: ['$userTrustNote', baseChangePercentage] }] }]},
+              else: { $min: [maxTrustNote, { $add: ['$userTrustNote', { $multiply: ['$userTrustNote', baseChangePercentage] }] }]}
+            },
+          },
+        },
+      },
+    ]);
 
     return res.status(200).json({
       message: 'Reviews Posted successfully',
