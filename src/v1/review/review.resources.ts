f6aef8495d75888df417357da52d87a631735bb1
kapilkumartomar <kapil.upwork@gmail.com>
2024-03-18 13:25:13 +0530

    update

--- src/v1/review/review.resources.ts
+++ src/v1/review/review.resources.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import Review from './review.model';
 
 export async function findReview(query: { reviewed?: Number, eventId?: string, userReveiwBy?: string }) {
@@ -10,4 +11,27 @@ export async function findOneReview(query: { eventId: string, userReviewBy: stri
 
 export async function postReview(payload: { [key: string]: string | boolean }) {
   return Review.create(payload);
+}
+
+export async function eventReviewAverage({ eventId }: { eventId: string }) {
+  return Review.aggregate([
+    {
+      $match: {
+        eventId: new mongoose.Types.ObjectId(eventId),
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalReviewedEvents: { $sum: 1 },
+        averageEventReview: { $avg: '$review' },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        averageEventReview: 1,
+      },
+    },
+  ]);
 }
